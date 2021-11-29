2a985411bdd508fd3ee728a0bc27f8ae00132546
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-11-29 19:03:55 +0530

    Add files via upload

---
+++ circular ring.py
@@ -0,0 +1,17 @@
+# Yash Gupta...
+import turtle
+t= turtle.Turtle()
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.speed(-2)
+print("Yash Gupta...")
+
+while (True):
+    for i in range(6):
+        for colour in ["red","blue","brown","green","grey","pink"]:
+            turtle.color(colour)
+            turtle.circle(100)
+            turtle.left(33)
+
+turtle.hideturtle()
+turtle.mainloop()

b610e936d2f6836bc89d55ecb9bdf262942ba7b6
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-11-30 22:11:07 +0530

    Update Calculate the GST Price.py

--- Calculate the GST Price.py
+++ Calculate the GST Price.py
@@ -2,7 +2,6 @@ cp=float(input())
 cgst=float(input('Enter the percentage(%) of CGST Rate =>'))
 sgst=float(input('Enter the percentage(%) of SGST Rate =>'))
 #(CGST Tax Rate on product) + (SGST Tax Rate on product)
-
 ta=0 
 Tcgst=((cgst/100)*cp)
 Tsgst=((sgst/100)*cp)

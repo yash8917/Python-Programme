0d742f36649bcbabf866fb28af1f957b76899902
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-11-30 22:09:50 +0530

    Add files via upload

---
+++ l.min.py
@@ -0,0 +1,12 @@
+# l=[12,34,546,4,456]
+# min=l[0]
+# for i in l:
+#     if i<min:
+#         min=i
+# print(min)
+l=[2232,'yash','huraah',121]
+a=0
+for i in l:
+    if len(i) > 1 and i[0] == i[-1]:
+        a+=1
+    print(a)

31d82775756dc4e60285ad7b2a745c41a30b7532
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-12-01 19:22:54 +0530

    Add files via upload

---
+++ l.remove  duplicate ele.py
@@ -0,0 +1,14 @@
+# l1=[1,2,3,2,3,5,4,1]
+# l2=[]
+# for i in l1:
+#     if i not in l2:
+#         l2.append(i)
+# print('''After the Removing the duplicate element's List will be a =>''',l2)
+# #l2.short()
+# print(l2.short())
+l=['yash',120,'hi',30,10]
+su=0
+for i in l:
+    if type(i)== int:
+        su+=i
+print(su)

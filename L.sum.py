6c9bef945e617024e3d0c2275801d16416df6465
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-12-02 22:13:13 +0530

    Add files via upload

---
+++ L.sum.py
@@ -0,0 +1,5 @@
+# l=[1,2,3,4,5,6]
+# print('The sum of the List is :',sum(l))
+#-------------------------------------------------#
+l=[1,2,3,4,1.22232345,5,6]
+print('The sum of the List is :',format(sum(l),'.2f'))

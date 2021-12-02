2a47b453268f10d75a3a54d6dc0e2ce6827d59ae
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-12-02 22:12:52 +0530

    Add files via upload

---
+++ l.search every ele how many times occurance in the list.py
@@ -0,0 +1,31 @@
+'''l=[1,3,4,1,'yash',1,4,3,'yash',2,4,5]
+l1=[]
+for i in l:
+    if i not in l1:
+        k=l1.append(i)
+print(l1)
+c=0
+for j in l1:
+    if j in l:
+        k=l.count(j)
+      # c+=1'''
+   # print(j,'''occuranced in the list :''',k,'''time's..''')
+   '''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+# ............................................#
+print('Hi Amit\nI have somthing to you.\n')
+n=input('->')
+
+print('''Kanchan Love's you but she can't explain you..''')
+print ('Do you love Kanchan ?')
+m=input ('->')
+print('''Congratulation's! Amit.)
+   
+   
+
